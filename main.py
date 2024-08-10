4cfc1b70f75e0886340f31081c7e5d7878c82486
Orel Kanditan <orelkanditan@gmail.com>
2024-08-10 20:18:12 +0300

    Added simple naive score (0.53) and simple model (0.58)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from data.fbref_scraper import scrap_fbref
 from db import sqlite_client
 from service.data_organizer import add_players_data, agg_prev_games, fix_xscore
@@ -14,6 +15,14 @@ def scrap_data(db_client, retries):
         if retries < MAX_RETRIES:
             scrap_data(db_client, retries)
 
+def naive_score():
+    df = pd.read_csv('output/matches_with_players.csv')
+
+    # Calculate accuracy
+    accuracy = (df['Score'] == df['xScore']).mean()
+
+    # Print the accuracy
+    print(f"Baseline Accuracy: {accuracy:.2%}")
 
 def main():
     # db_client = sqlite_client.SQLiteClient()
@@ -21,7 +30,8 @@ def main():
     # scrap_data(db_client, 0)
     # add_players_data(db_client)
     # agg_prev_games()
-    fix_xscore()
+    # fix_xscore()
+    naive_score()
 
 if __name__ == '__main__':
     main()
